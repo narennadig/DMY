689a71adfbbcfde08ad3b301c7c4682613ed8b9b
Narendranath D Nadig <35164475+narennadig@users.noreply.github.com>
2019-01-02 16:15:03 +0530

    Update 3.r

--- R/3.r
+++ R/3.r
@@ -1,8 +1,8 @@
 library(arules)
-#library(arulesViz)
+library(arulesViz)
 patterns=random.patterns(nItems=1000)
 trans=random.transactions(nItems=1000,nTrans=1000,method="agrawal",patterns=patterns)
 rules=apriori(trans,parameter = list(support=0.01,confidence=0.5))
 print(inspect(rules))
-#arulesViz(rules,method="grouped")
+arulesViz(rules,method="grouped")
 
